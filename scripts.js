3696cb5d00b082e3c3e888a71612b4e63a2c75a4
TristanDaniels <tristandaniels07@gmail.com>
2022-06-28 16:17:56 +0200

    update

--- scripts.js
+++ scripts.js
@@ -1,7 +1,58 @@
-// let propertyList [{
-//     image: "",
-//     title: "",
-//     price: "",
-//     sqFt: "",
-// },
-// ]
+let properties = [
+  {
+    image: "",
+    title: "",
+    type: "House",
+    location: "Cape Town",
+    price: "",
+    population: "10",
+  },
+  {
+    image: "",
+    title: "",
+    type: "",
+    location: "Johannesburg",
+    price: "",
+    population: "12",
+  },
+  {
+    image: "",
+    title: "",
+    type: "",
+    location: "Bloemfontein",
+    price: "",
+    population: "8",
+  },
+  {
+    image: "",
+    title: "",
+    type: "",
+    location: "Pretoria",
+    price: "",
+    population: "8",
+  },
+  {
+    image: "",
+    title: "",
+    type: "",
+    location: "Cape Town",
+    price: "",
+    population: "6",
+  },
+  {
+    image: "",
+    title: "",
+    type: "",
+    location: "Bloemfontein",
+    price: "",
+    population: "10",
+  },
+];
+function showProperties(properties) {
+  document.querySelector("#propertyList").innerHTML = "";
+  pokemonArray.forEach((properties) => {
+    document.querySelector("#propertyList").innerHTML += `
+      <img src=${image} alt="Front">
+       `;
+  });
+}
