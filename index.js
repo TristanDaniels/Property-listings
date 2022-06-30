5f25b268112a8c5cec04bd35af2ffda52279aa45
TristanDaniels <tristandaniels07@gmail.com>
2022-06-30 15:03:26 +0200

    update

--- index.js
+++ index.js
@@ -1,163 +1,174 @@
-let properties = [
-  {
-    id: "1",
-    image: "https://i.postimg.cc/RV0GzSCX/Home1.jpg",
-    title: "Luxury Home",
-    type: "House",
-    location: "96 Apple Street, Cape Town, 7802, Western Cape, South Africa",
-    price: "R1 200 000",
-    bedrooms: "5",
-    bathrooms: "3",
-    carspace: "4",
-    population: "10",
-  },
-  {
-    id: "2",
-    image: "https://i.postimg.cc/MGnhZ0W3/Home2.jpg",
-    title: "Luxury Home",
-    type: "House",
-    location: "27 Peach Street, Johannesburg, 5570, Gauteng, South Africa",
-    price: "R1 300 000",
-    bedrooms: "6",
-    bathrooms: "3",
-    carspace: "4",
-    population: "12",
-  },
-  {
-    id: "3",
-    image: "https://i.postimg.cc/Zq6xL0jK/Home3.jpg",
-    title: "Luxury Home",
-    type: "House",
-    location:
-      "92 Strawberry Road, Bloemfontein, 8700, Free State, South Africa",
-    price: "R800 000",
-    bedrooms: "4",
-    bathrooms: "2",
-    carspace: "2",
-    population: "8",
-  },
-  {
-    id: 4,
-    image: "https://i.postimg.cc/zXMwZTcG/Home4.jpg",
-    title: "Luxury Home",
-    type: "House",
-    location: "77 Bananna Ave, Pretoria, 5200, Gauteng, South Africa",
-    price: "R950 000",
-    bedrooms: "4",
-    bathrooms: "2",
-    carspace: "4",
-    population: "8",
-  },
-  {
-    id: 5,
-    image: "https://i.postimg.cc/T3wsQjW2/Apartment-1.jpg",
-    title: "Apartment",
-    type: "Apartment",
-    location: "10 True Estate, Cape Town, 7800, Western Cape, South Africa",
-    price: "R700 000",
-    bedrooms: "2",
-    bathrooms: "1",
-    carspace: "1",
-    population: "4",
-  },
-  {
-    id: 6,
-    image: "https://i.postimg.cc/zvyL0c5S/Apartment-2.jpg",
-    title: "Apartment",
-    type: "Apartment",
-    location: "11 Lucky Village, Bloemfontein, 8800, Freestate, South Africa",
-    price: "R400 000",
-    bedrooms: "1",
-    bathrooms: "1",
-    carspace: "1",
-    population: "2",
-  },
-  {
-    id: 7,
-    image: "https://i.postimg.cc/L53WQNT5/Apartment-3.jpg",
-    title: "Apartment",
-    type: "Apartment",
-    location:
-      "47 Faithful Complex, Bloemfontein, 8800, Freestate, South Africa",
-    price: "R550 000",
-    bedrooms: "2",
-    bathrooms: "1",
-    carspace: "1",
-    population: "4",
-  },
-  {
-    id: 8,
-    image: "https://i.postimg.cc/D00GRzQG/Studio-1.jpg",
-    title: "Modern Day Studio",
-    type: "Studio",
-    location: "55 Tulip Village, Pretoria, 5100, Gauteng, South Africa",
-    price: "R300 000",
-    bedrooms: "1",
-    bathrooms: "1",
-    carspace: "1",
-    population: "2",
-  },
-  {
-    id: 9,
-    image: "https://i.postimg.cc/CxTZs72k/Studio-2.jpg",
-    title: "Musical Studio",
-    type: "Studio",
-    location: "78 Mellow Village, Johannesburg, 5800, Gauteng, South Africa",
-    price: "R450 000",
-    bedrooms: "1",
-    bathrooms: "1",
-    carspace: "1",
-    population: "2",
-  },
-  {
-    id: 10,
-    image: "https://i.postimg.cc/4NKt5mxk/office1.jpg",
-    title: "Modern Day Office",
-    type: "Office",
-    location:
-      "124 Green Point Village, Cape Town, 7500, Western Cape, South Africa",
-    price: "R800 000",
-    bedrooms: "1",
-    bathrooms: "1",
-    carspace: "1",
-    population: "2",
-  },
-];
+let properties = JSON.parse(localStorage.getItem("properties"))
+  ? JSON.parse(localStorage.getItem("properties"))
+  : [
+      {
+        id: 1,
+        image: "https://i.postimg.cc/RV0GzSCX/Home1.jpg",
+        title: "Luxury Home",
+        type: "House",
+        location:
+          "96 Apple Street, Cape Town, 7802, Western Cape, South Africa",
+        price: "R1 200 000",
+        bedrooms: "5",
+        bathrooms: "3",
+        carspace: "4",
+        population: "10",
+      },
+      {
+        id: 2,
+        image: "https://i.postimg.cc/MGnhZ0W3/Home2.jpg",
+        title: "Luxury Home",
+        type: "House",
+        location: "27 Peach Street, Johannesburg, 5570, Gauteng, South Africa",
+        price: "R1 300 000",
+        bedrooms: "6",
+        bathrooms: "3",
+        carspace: "4",
+        population: "12",
+      },
+      {
+        id: 3,
+        image: "https://i.postimg.cc/Zq6xL0jK/Home3.jpg",
+        title: "Luxury Home",
+        type: "House",
+        location:
+          "92 Strawberry Road, Bloemfontein, 8700, Free State, South Africa",
+        price: "R800 000",
+        bedrooms: "4",
+        bathrooms: "2",
+        carspace: "2",
+        population: "8",
+      },
+      {
+        id: 4,
+        image: "https://i.postimg.cc/zXMwZTcG/Home4.jpg",
+        title: "Luxury Home",
+        type: "House",
+        location: "77 Bananna Ave, Pretoria, 5200, Gauteng, South Africa",
+        price: "R950 000",
+        bedrooms: "4",
+        bathrooms: "2",
+        carspace: "4",
+        population: "8",
+      },
+      {
+        id: 5,
+        image: "https://i.postimg.cc/T3wsQjW2/Apartment-1.jpg",
+        title: "Apartment",
+        type: "Apartment",
+        location: "10 True Estate, Cape Town, 7800, Western Cape, South Africa",
+        price: "R700 000",
+        bedrooms: "2",
+        bathrooms: "1",
+        carspace: "1",
+        population: "4",
+      },
+      {
+        id: 6,
+        image: "https://i.postimg.cc/zvyL0c5S/Apartment-2.jpg",
+        title: "Apartment",
+        type: "Apartment",
+        location:
+          "11 Lucky Village, Bloemfontein, 8800, Freestate, South Africa",
+        price: "R400 000",
+        bedrooms: "1",
+        bathrooms: "1",
+        carspace: "1",
+        population: "2",
+      },
+      {
+        id: 7,
+        image: "https://i.postimg.cc/L53WQNT5/Apartment-3.jpg",
+        title: "Apartment",
+        type: "Apartment",
+        location:
+          "47 Faithful Complex, Bloemfontein, 8800, Freestate, South Africa",
+        price: "R550 000",
+        bedrooms: "2",
+        bathrooms: "1",
+        carspace: "1",
+        population: "4",
+      },
+      {
+        id: 8,
+        image: "https://i.postimg.cc/D00GRzQG/Studio-1.jpg",
+        title: "Modern Day Studio",
+        type: "Studio",
+        location: "55 Tulip Village, Pretoria, 5100, Gauteng, South Africa",
+        price: "R300 000",
+        bedrooms: "1",
+        bathrooms: "1",
+        carspace: "1",
+        population: "2",
+      },
+      {
+        id: 9,
+        image: "https://i.postimg.cc/CxTZs72k/Studio-2.jpg",
+        title: "Musical Studio",
+        type: "Studio",
+        location:
+          "78 Mellow Village, Johannesburg, 5800, Gauteng, South Africa",
+        price: "R450 000",
+        bedrooms: "1",
+        bathrooms: "1",
+        carspace: "1",
+        population: "2",
+      },
+      {
+        id: 10,
+        image: "https://i.postimg.cc/4NKt5mxk/office1.jpg",
+        title: "Modern Day Office",
+        type: "Office",
+        location:
+          "124 Green Point Village, Cape Town, 7500, Western Cape, South Africa",
+        price: "R800 000",
+        bedrooms: "1",
+        bathrooms: "1",
+        carspace: "1",
+        population: "2",
+      },
+    ];
 
 const propertiesTable = document.querySelector("#propertiesbox");
-function showProperties() {
+function showProperties(properties) {
   document.querySelector("#propertiesbox").innerHTML = "";
   properties.forEach((property) => {
     propertiesTable.innerHTML += `
-        <tr class="d-flex flex-row">
-          <td><img src=${property.image}></td>
-          <td>${property.title}</td>
-          <td>${property.location}</td>
-          <td>${property.type}</td>
-          <td>${property.bedrooms}</td>
-          <td>${property.bathrooms}</td>
-          <td>${property.carspace}</td>
-          <td>${property.population}</td>
-          <td>${property.price}</td>
-          <td><button onclick="deleteproperty(${property.id})" ><i class="fa-solid fa-trash"></i></button></td>
-          <td><button onclick="editproperty(${property.id})"><i class="fa-solid fa-pen-to-square"></i></button></td>
-        <tr>
+        <div class="d-flex flex-row">
+          <img class="w-25 mt-3 mb-3 ms-3 me-3"src=${property.image}>
+          <div class="d-flex flex-column">
+          <div class="d-flex flex-column">
+          <div class="ms-2">${property.title}</div>
+          <div class="ms-2">${property.location}</div>
+          <div class="ms-2">${property.price}</div>
+          <div class="ms-2"><i class="fa-solid fa-bed me-2"></i>${property.bedrooms}</div>
+          <div class="ms-2"><i class="fa-solid fa-shower me-2"></i>${property.bathrooms}</div>
+          <div class="ms-2"><i class="fa-solid fa-car me-2"></i>${property.carspace}</div>
+          <div class="ms-2"><i class="fa-solid fa-person me-2"></i>${property.population}</div>
+          <div class="ms-2">${property.type}</div>
+          </div>
+          <div class="d-flex flex-row">
+          <div><button class="btn" onclick="deleteProperty(${property.id})" ><i class="fa-solid fa-trash"></i></button></div>
+          <div><button class="btn"onclick="editProperty(${property.id})"><i class="fa-solid fa-pen-to-square"></i></button></div>
+          </div>
+          </div>
+        </div>
       `;
   });
 }
-showProperties();
-
 showProperties(properties);
-function deleteProperties(id) {
-  properties = properties.filter((listing) => {
+
+function deleteProperty(id) {
+  properties = properties.filter((property) => {
     //Goes through the array and filters out every id that is equal to the selected one
     return property.id !== id; //Returns all objects in the array that is not the same id as the selected one
   });
   localStorage.setItem("properties", JSON.stringify(properties));
   showProperties(properties); //Displays the array
 }
-function editproperty(id) {
-  let property = property.find((property) => property.id === id); //Finds the id of button that was clicked
+
+function editProperty(id) {
+  let property = properties.find((property) => property.id === id); //Finds the id of button that was clicked
   let input = prompt("Input Changes"); //Allows the user to input their own changes
   property.title = input; //Changes the content to what the user's input
   localStorage.setItem("properties", JSON.stringify(properties));
--- scripts.js
+++ scripts.js
@@ -1,128 +1,153 @@
-let properties = [
-  {
-    id: "1",
-    image: "https://i.postimg.cc/RV0GzSCX/Home1.jpg",
-    title: "Luxury Home",
-    type: "House",
-    location: "96 Apple Street, Cape Town, 7802, Western Cape, South Africa",
-    price: "R1 200 000",
-    bedrooms: "5",
-    bathrooms: "3",
-    carspace: "4",
-    population: "10",
-  },
-  {
-    id: "2",
-    image: "https://i.postimg.cc/MGnhZ0W3/Home2.jpg",
-    title: "Luxury Home",
-    type: "House",
-    location: "27 Peach Street, Johannesburg, 5570, Gauteng, South Africa",
-    price: "R1 300 000",
-    bedrooms: "6",
-    bathrooms: "3",
-    carspace: "4",
-    population: "12",
-  },
-  {
-    id: "3",
-    image: "https://i.postimg.cc/Zq6xL0jK/Home3.jpg",
-    title: "Luxury Home",
-    type: "House",
-    location:
-      "92 Strawberry Road, Bloemfontein, 8700, Free State, South Africa",
-    price: "R800 000",
-    bedrooms: "4",
-    bathrooms: "2",
-    carspace: "2",
-    population: "8",
-  },
-  {
-    id: 4,
-    image: "https://i.postimg.cc/zXMwZTcG/Home4.jpg",
-    title: "Luxury Home",
-    type: "House",
-    location: "77 Bananna Ave, Pretoria, 5200, Gauteng, South Africa",
-    price: "R950 000",
-    bedrooms: "4",
-    bathrooms: "2",
-    carspace: "4",
-    population: "8",
-  },
-  {
-    id: 5,
-    image: "https://i.postimg.cc/T3wsQjW2/Apartment-1.jpg",
-    title: "Apartment",
-    type: "Apartment",
-    location: "10 True Estate, Cape Town, 7800, Western Cape, South Africa",
-    price: "R700 000",
-    bedrooms: "2",
-    bathrooms: "1",
-    carspace: "1",
-    population: "4",
-  },
-  {
-    id: 6,
-    image: "https://i.postimg.cc/zvyL0c5S/Apartment-2.jpg",
-    title: "Apartment",
-    type: "Apartment",
-    location: "11 Lucky Village, Bloemfontein, 8800, Freestate, South Africa",
-    price: "R400 000",
-    bedrooms: "1",
-    bathrooms: "1",
-    carspace: "1",
-    population: "2",
-  },
-  {
-    id: 7,
-    image: "https://i.postimg.cc/L53WQNT5/Apartment-3.jpg",
-    title: "Apartment",
-    type: "Apartment",
-    location:
-      "47 Faithful Complex, Bloemfontein, 8800, Freestate, South Africa",
-    price: "R550 000",
-    bedrooms: "2",
-    bathrooms: "1",
-    carspace: "1",
-    population: "4",
-  },
-  {
-    id: 8,
-    image: "https://i.postimg.cc/D00GRzQG/Studio-1.jpg",
-    title: "Modern Day Studio",
-    type: "Studio",
-    location: "55 Tulip Village, Pretoria, 5100, Gauteng, South Africa",
-    price: "R300 000",
-    bedrooms: "1",
-    bathrooms: "1",
-    carspace: "1",
-    population: "2",
-  },
-  {
-    id: 9,
-    image: "https://i.postimg.cc/CxTZs72k/Studio-2.jpg",
-    title: "Musical Studio",
-    type: "Studio",
-    location: "78 Mellow Village, Johannesburg, 5800, Gauteng, South Africa",
-    price: "R450 000",
-    bedrooms: "1",
-    bathrooms: "1",
-    carspace: "1",
-    population: "2",
-  },
-  {
-    id: 10,
-    image: "https://i.postimg.cc/4NKt5mxk/office1.jpg",
-    title: "Modern Day Office",
-    type: "Office",
-    location:
-      "124 Green Point Village, Cape Town, 7500, Western Cape, South Africa",
-    price: "R800 000",
-    bedrooms: "1",
-    bathrooms: "1",
-    carspace: "1",
-    population: "2",
-  },
-];
+let properties = JSON.parse(localStorage.getItem("properties"))
+  ? JSON.parse(localStorage.getItem("properties"))
+  : [
+      {
+        id: 1,
+        image: "https://i.postimg.cc/RV0GzSCX/Home1.jpg",
+        title: "Luxury Home",
+        type: "House",
+        location:
+          "96 Apple Street, Cape Town, 7802, Western Cape, South Africa",
+        city: "Cape Town",
+        price: "R1 200 000",
+        value: "R900 000 - R1 499 999",
+        bedrooms: "5",
+        bathrooms: "3",
+        carspace: "4",
+        population: "10",
+      },
+      {
+        id: 2,
+        image: "https://i.postimg.cc/MGnhZ0W3/Home2.jpg",
+        title: "Luxury Home",
+        type: "House",
+        location: "27 Peach Street, Johannesburg, 5570, Gauteng, South Africa",
+        city: "Johannesburg",
+        price: "R1 300 000",
+        value: "R900 000 - R1 499 999",
+        bedrooms: "6",
+        bathrooms: "3",
+        carspace: "4",
+        population: "12",
+      },
+      {
+        id: 3,
+        image: "https://i.postimg.cc/Zq6xL0jK/Home3.jpg",
+        title: "Luxury Home",
+        type: "House",
+        location:
+          "92 Strawberry Road, Bloemfontein, 8700, Free State, South Africa",
+        city: "Bloemfontein",
+        price: "R800 000",
+        value: "R750 000 - R899 999",
+        bedrooms: "4",
+        bathrooms: "2",
+        carspace: "2",
+        population: "8",
+      },
+      {
+        id: 4,
+        image: "https://i.postimg.cc/zXMwZTcG/Home4.jpg",
+        title: "Luxury Home",
+        type: "House",
+        location: "77 Bananna Ave, Pretoria, 5200, Gauteng, South Africa",
+        city: "Pretoria",
+        price: "R950 000",
+        value: "R900 000 - R1 499 999",
+        bedrooms: "4",
+        bathrooms: "2",
+        carspace: "4",
+        population: "8",
+      },
+      {
+        id: 5,
+        image: "https://i.postimg.cc/T3wsQjW2/Apartment-1.jpg",
+        title: "Apartment",
+        type: "Apartment",
+        location: "10 True Estate, Cape Town, 7800, Western Cape, South Africa",
+        city: "Cape Town",
+        price: "R700 000",
+        value: "R500 000 - R749 999",
+        bedrooms: "2",
+        bathrooms: "1",
+        carspace: "1",
+        population: "4",
+      },
+      {
+        id: 6,
+        image: "https://i.postimg.cc/zvyL0c5S/Apartment-2.jpg",
+        title: "Apartment",
+        type: "Apartment",
+        location:
+          "11 Lucky Village, Bloemfontein, 8800, Freestate, South Africa",
+        city: "Bloemfontein",
+        price: "R400 000",
+        value: "R350 000 - R499 999",
+        bedrooms: "1",
+        bathrooms: "1",
+        carspace: "1",
+        population: "2",
+      },
+      {
+        id: 7,
+        image: "https://i.postimg.cc/L53WQNT5/Apartment-3.jpg",
+        title: "Apartment",
+        type: "Apartment",
+        location:
+          "47 Faithful Complex, Bloemfontein, 8800, Freestate, South Africa",
+        city: "Bloemfontein",
+        price: "R550 000",
+        value: "R500 000 - R749 999",
+        bedrooms: "2",
+        bathrooms: "1",
+        carspace: "1",
+        population: "4",
+      },
+      {
+        id: 8,
+        image: "https://i.postimg.cc/D00GRzQG/Studio-1.jpg",
+        title: "Modern Day Studio",
+        type: "Studio",
+        location: "55 Tulip Village, Pretoria, 5100, Gauteng, South Africa",
+        city: "Pretoria",
+        price: "R300 000",
+        value: "R200 000 - R349 999",
+        bedrooms: "1",
+        bathrooms: "1",
+        carspace: "1",
+        population: "2",
+      },
+      {
+        id: 9,
+        image: "https://i.postimg.cc/CxTZs72k/Studio-2.jpg",
+        title: "Musical Studio",
+        type: "Studio",
+        location:
+          "78 Mellow Village, Johannesburg, 5800, Gauteng, South Africa",
+        city: "Johannesburg",
+        price: "R450 000",
+        value: "R350 000 - R499 999",
+        bedrooms: "1",
+        bathrooms: "1",
+        carspace: "1",
+        population: "2",
+      },
+      {
+        id: 10,
+        image: "https://i.postimg.cc/4NKt5mxk/office1.jpg",
+        title: "Modern Day Office",
+        type: "Office",
+        location:
+          "124 Green Point Village, Cape Town, 7500, Western Cape, South Africa",
+        city: "Cape Town",
+        price: "R800 000",
+        value: "R750 000 - R899 999",
+        bedrooms: "1",
+        bathrooms: "1",
+        carspace: "1",
+        population: "2",
+      },
+    ];
 
 const forSaleContainer = document.getElementById("boxes");
 function showProperties() {
@@ -130,6 +155,37 @@ function showProperties() {
   properties.forEach((property) => {
     forSaleContainer.innerHTML += `
         <div class="card shadow mb-4">
+          <img src=${property.image} class="car-img mb-4 img-fluid" alt="Picture of Property" />
+          <h5 class="text-black">${property.title}</h5>
+          <p class="text-secondary">
+            ${property.location}
+          </p> 
+          <p>${property.price}</p>
+          <div class="d-flex flex-row justify-content-center">
+            <i class="fa-solid fa-bed me-2"></i>${property.bedrooms}
+            <i class="fa-solid fa-shower me-2 ms-2"></i>${property.bathrooms}
+            <i class="fa-solid fa-car me-2 ms-2"></i>${property.carspace}
+            <i class="fa-solid fa-person me-2 ms-2"></i>${property.population}
+          </div>
+          <p class="mt-2">${property.type}</p>
+            `;
+  });
+}
+showProperties();
+
+// Filtering functions
+filter = () => {
+  let FilterOption = document.querySelector("#type").value;
+  if (FilterOption !== "all") {
+    console.log(FilterOption);
+    document.querySelector("#boxes").innerHTML = "";
+    let specificView = properties.filter(
+      (property) => property.type == FilterOption
+    );
+    specificView.forEach((property) => {
+      document.querySelector(
+        "#boxes"
+      ).innerHTML += `<div class="card shadow mb-4">
           <img src=${property.image} class="car-img mb-4 img-fluid" alt="Picture of Property" />
           <h5 class="text-black">${property.title}</h5>
           <p class="text-secondary">
@@ -144,6 +200,112 @@ function showProperties() {
           </div>
           <p class="mt-2">${property.type}</p>
             `;
-  });
-}
-showProperties();
+
+      console.log(specificView);
+    });
+  } else {
+    display();
+  }
+};
+
+filter2 = () => {
+  let FilterOption = document.querySelector("#location").value;
+  if (FilterOption !== "all") {
+    console.log(FilterOption);
+    document.querySelector("#boxes").innerHTML = "";
+    let specificView = properties.filter(
+      (property) => property.city == FilterOption
+    );
+    specificView.forEach((property) => {
+      document.querySelector(
+        "#boxes"
+      ).innerHTML += `<div class="card shadow mb-4">
+          <img src=${property.image} class="car-img mb-4 img-fluid" alt="Picture of Property" />
+          <h5 class="text-black">${property.title}</h5>
+          <p class="text-secondary">
+            ${property.location}
+          </p>
+          <p>${property.price}</p>
+          <div class="d-flex flex-row justify-content-center">
+            <i class="fa-solid fa-bed me-2"></i>${property.bedrooms}
+            <i class="fa-solid fa-shower me-2 ms-2"></i>${property.bathrooms}
+            <i class="fa-solid fa-car me-2 ms-2"></i>${property.carspace}
+            <i class="fa-solid fa-person me-2 ms-2"></i>${property.population}
+          </div>
+          <p class="mt-2">${property.type}</p>
+            `;
+
+      console.log(specificView);
+    });
+  } else {
+    display();
+  }
+};
+
+filter3 = () => {
+  let FilterOption = document.querySelector("#bedrooms").value;
+  if (FilterOption !== "all") {
+    console.log(FilterOption);
+    document.querySelector("#boxes").innerHTML = "";
+    let specificView = properties.filter(
+      (property) => property.bedrooms == FilterOption
+    );
+    specificView.forEach((property) => {
+      document.querySelector(
+        "#boxes"
+      ).innerHTML += `<div class="card shadow mb-4">
+          <img src=${property.image} class="car-img mb-4 img-fluid" alt="Picture of Property" />
+          <h5 class="text-black">${property.title}</h5>
+          <p class="text-secondary">
+            ${property.location}
+          </p>
+          <p>${property.price}</p>
+          <div class="d-flex flex-row justify-content-center">
+            <i class="fa-solid fa-bed me-2"></i>${property.bedrooms}
+            <i class="fa-solid fa-shower me-2 ms-2"></i>${property.bathrooms}
+            <i class="fa-solid fa-car me-2 ms-2"></i>${property.carspace}
+            <i class="fa-solid fa-person me-2 ms-2"></i>${property.population}
+          </div>
+          <p class="mt-2">${property.type}</p>
+            `;
+
+      console.log(specificView);
+    });
+  } else {
+    display();
+  }
+};
+
+filter4 = () => {
+  let FilterOption = document.querySelector("#budget").value;
+  if (FilterOption !== "all") {
+    console.log(FilterOption);
+    document.querySelector("#boxes").innerHTML = "";
+    let specificView = properties.filter(
+      (property) => property.value == FilterOption
+    );
+    specificView.forEach((property) => {
+      document.querySelector(
+        "#boxes"
+      ).innerHTML += `<div class="card shadow mb-4">
+          <img src=${property.image} class="car-img mb-4 img-fluid" alt="Picture of Property" />
+          <h5 class="text-black">${property.title}</h5>
+          <p class="text-secondary">
+            ${property.location}
+          </p>
+          <p>${property.price}</p>
+          <div class="d-flex flex-row justify-content-center">
+            <i class="fa-solid fa-bed me-2"></i>${property.bedrooms}
+            <i class="fa-solid fa-shower me-2 ms-2"></i>${property.bathrooms}
+            <i class="fa-solid fa-car me-2 ms-2"></i>${property.carspace}
+            <i class="fa-solid fa-person me-2 ms-2"></i>${property.population}
+          </div>
+          <p class="mt-2">${property.type}</p>
+            `;
+
+      console.log(specificView);
+    });
+  } else {
+    display();
+  }
+};
